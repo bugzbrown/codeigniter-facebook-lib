557b65c7cc2db1a4adc804ae2d1e5a241272064d
Greg <gregory.brown@vbeta.com.br>
2013-03-25 15:57:52 -0300

    initial commit

---
+++ facebook.config.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['fbAppId'] = 'APP_ID';
+$config['fbSecret'] = 'SECRET';
+$config['fbCanvas'] = 'https://apps.facebook.com/APP_ID/';
+$config['fbScope'] = 'read_stream, email, friends_likes';
+$config['fbDisplay'] = 'page';
