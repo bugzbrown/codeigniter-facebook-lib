82626a4e3b7e7b7a29b65c1532aa9d1bc3118943
Greg <gregory.brown@wikitoria.com.br>
2013-04-10 09:48:24 -0300

    Added cookie for storing signed request to allow multiple page sites. Sessions in codeigniter don't treat the signedrequest properly and hence screw up navigation.

--- Fb.php
+++ Fb.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
-require_once 'facebook.php';
+require_once 'fb-php-sdk/facebook.php';
 class Fb {
 	private $fb_app_id = '';
 	private $fb_app_secret = '';
@@ -7,8 +7,13 @@ class Fb {
 
 	public $user;
 	public $logout_url;
-
+	private function signedRequest(){
+		$sr = isset($_REQUEST['signed_request'])?$_REQUEST['signed_request']:'';
+		setcookie("fb_signed_request_val", $sr, time()+(60*60*24),'/',$_SERVER['SERVER_NAME'],1);
+		set_cookie($cookie);		
+	}
     public function init_facebook(){
+    	$this->signedRequest();
     	$this->load_config_file();
     	$this->get_fb_object();
     	if (!$this->have_user()){
